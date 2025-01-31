a79ec51202272a1d8a095437441ba8bd9d732baf
M Kazim <mkaazim13@gmail.com>
2025-01-31 21:40:05 +0500

    fix bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,17 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL = '/api';  // Use the proxy
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL='https://api.themoviedb.org/3';
 
-export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-  const data = await response.json();
-  return data.results;
-};
+export const getPopularMovies= async ()=>{
+    const response=await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data=await response.json()
+    return data.results;
 
-export const searchMovies = async (query) => {
-  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-  const data = await response.json();
-  console.log(data.results);
-  return data.results;
-};
+}
+export const searchMovies= async (query)=>{
+    const response=await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data=await response.json()
+    console.log(data.results);
+
+    return data.results;
+
+}
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.themoviedb.org/3', // The target TMDB API
-        changeOrigin: true, // This ensures the proxy works correctly
-        rewrite: (path) => path.replace(/^\/api/, '') // Remove '/api' prefix before sending the request
-      }
-    }
-  }
+  plugins: [react(),tailwindcss(),],
 })
