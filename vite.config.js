0a4916fe8f50021ee57e990c382d399fc2985772
M Kazim <mkaazim13@gmail.com>
2025-01-31 21:22:54 +0500

    solve CORS issue for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.themoviedb.org/3', // The target TMDB API
+        changeOrigin: true, // This ensures the proxy works correctly
+        rewrite: (path) => path.replace(/^\/api/, '') // Remove '/api' prefix before sending the request
+      }
+    }
+  }
 })
