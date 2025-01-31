cb1d95c4f0db64159e2b05d753483dd6976f264c
M Kazim <mkaazim13@gmail.com>
2025-01-31 20:38:23 +0500

    add framer-motion on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import Favorites from "./pages/Favorites"
 import Home from "./pages/Home"
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, useLocation} from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import { MovieProvider } from "./context/MovieContext"
+import {motion} from 'framer-motion'
 
 function App() {
+
+  const location=useLocation();
+
   return (
+
     <MovieProvider>
     <div className="min-h-screen bg-gray-900">
       <Navbar />
       <main className="container mx-auto px-4 py-8">
-        <Routes>
+      <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+          >
+        <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home/>}/>
           <Route path="/favorites" element={<Favorites/>}/>
         </Routes>
+        </motion.div>
       </main>
     </div>
     </MovieProvider>
