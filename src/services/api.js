e71fdd96e33f57f3ebc66ab849de31388c3fe230
M Kazim <mkaazim13@gmail.com>
2025-01-31 20:18:24 +0500

    add api key protection

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_KEY='ab558757ff13a37d67e121b1cf6709cb';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
 const BASE_URL='https://api.themoviedb.org/3';
 
 export const getPopularMovies= async ()=>{
